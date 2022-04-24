144972f34496fa62d201b9987cffb6f45b5626d2
Amit <55355831+amitdeo5@users.noreply.github.com>
2022-04-25 02:30:17 +0530

    header done

--- cryptospot/src/App.js
+++ cryptospot/src/App.js
@@ -1,23 +1,63 @@
+
+import { makeStyles } from "@material-ui/core";
 import HomePage from "./Pages/HomePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
 import Header from "./components/Header";
-import CoinPage from './Pages/CoinPage';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-   <BrowserRouter>
-
-<div>
-<Header />
-   <Routes>
-  <Route path="/" element={<HomePage/>} />
-  <Route path="/coins/:id" element={<CoinPage/>}/>
-  </Routes>
-</div>
-
-   </BrowserRouter>
-  );
-}
+    <BrowserRouter>
+ 
+ <div className={classes.App}>
+ <Header />
+    <Routes>
+   <Route path="/" element={<HomePage/>} />
+   <Route path="/coins/:id" element={<CoinPage/>}/>
+   </Routes>
+ </div>
+ 
+    </BrowserRouter>
+   );
+ }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
